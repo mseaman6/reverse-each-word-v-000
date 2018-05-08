d5890082403cf889bd84e87e115bca3d55e17021
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 14:13:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-#def reverse_each_word(string)
-#  array_of_string = string.split(" ")
-#  reversed_array = []
-#  reversed_array << array_of_string.each do |word|
-#    word.reverse
-#  end
-#  reversed_array.to_s(" ")
-#end
-
 def reverse_each_word(string)
-  new_array = string.split(" ").collect do |word|
+  array_of_string = string.split(" ")
+  reversed_array = []
+  reversed_array << array_of_string.each do |word|
     word.reverse
   end
-  new_array.join(" ")
+  reversed_array.to_s(" ")
 end
+
+#def reverse_each_word(string)
+#  new_array = string.split(" ").collect do |word|
+#    word.reverse
+#  end
+#  new_array.join(" ")
+#end
