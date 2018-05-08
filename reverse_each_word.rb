677a824e03eacaed5432d30ce5714b0be22fa0eb
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 13:53:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   reversed_array = []
-  reversed_array << string.split("").each do |word|
+  reversed_array << string.split(" ").each do |word|
     word.reverse
   end
   reversed_array
